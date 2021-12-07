7fa84b7e0d93325c0c52448cd9299f3f54511c29
joethyr <joethayr@gmail.com>
2021-12-06 22:11:00 -0700

    added merge sort method.

--- recursion.rb
+++ recursion.rb
@@ -16,5 +16,28 @@ def fibs_rec(num)
   fibs_rec(num-1) + fibs_rec(num-2)
 end
 
+def merge_sort(arr)
+  # base case
+  return arr if arr.length < 2
+
+  half = arr.length / 2
+  left = merge_sort(arr[0...half])
+  right = merge_sort(arr[half..-1])
+  merge(left, right)
+end
+
+def merge(left, right, sorted_arr = [])
+  until left.empty? || right.empty?
+    if left[0] < right[0]
+      sorted_arr << left.shift
+    else
+      sorted_arr << right.shift
+    end
+  end
+  sorted_arr + left + right
+end
+
+
 puts fibs(8)
 puts fibs_rec(8)
+p merge_sort([2, 4, 1, 6])
